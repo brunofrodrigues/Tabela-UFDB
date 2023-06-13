c38ef27ac88687406bb4bea7211f28e952f484b8
Bruno Ferreira Rodrigues <130567518+brunofrodrigues@users.noreply.github.com>
2023-06-13 18:11:47 -0300

    Estrutura básica da página

---
+++ ufdb.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Unidades Federativas do Brasil</title>
+</head>
+<body>
+
+    <header>
+        <h2>Desenvolvimento <i>Web</i></h2>
+    </header>
+
+    <main>
+        <h1>Lista Sintética de Unidades Federativas do Brasil</h1>
+    </main>
+
+    <footer>
+        <p>&copy; 2023 - Bruno Ferreira Rodrigues</p>
+    </footer>
+    
+</body>
+</html>
